a746089e524b7deaf9e09a5e48dbe2aed7e5f2ad
Joao Pedro Dias <jpdias@live.com.pt>
2015-02-26 17:07:21 +0000

    bd

--- BD/bd.sql
+++ BD/bd.sql
@@ -1,82 +1,108 @@
-DROP TABLE IF EXISTS `Item`,`Container`,`Formula`;
+DROP TABLE IF EXISTS `User`,`Item`,`Container`,`Formula`,`Fabrication`,`FabricationItem`,`Transfer`,`Leak`,`Buy`,`Sale`,`Supplier`,`Client`;
+
+CREATE TABLE `User`(
+  id INTEGER NOT NULL PRIMARY KEY,
+  username VARCHAR(120) NOT NULL,
+  user_password VARCHAR(120) NOT NULL
+);
+
+CREATE TABLE `Client`(
+  id INTEGER NOT NULL PRIMARY KEY,
+  name VARCHAR(120)
+);
+
+CREATE TABLE `Supplier`(
+  id INTEGER NOT NULL PRIMARY KEY,
+  name VARCHAR(120)
+);
+
 
 CREATE TABLE `Item`(
-    code INTEGER NOT NULL PRIMARY KEY,
-    name VARCHAR NOT NULL,
-    unit CHAR NOT NULL,
-    degree FLOAT,
-    return_rate FLOAT
+  code INTEGER NOT NULL PRIMARY KEY,
+  name VARCHAR(120) NOT NULL,
+  unit CHAR NOT NULL,
+  degree FLOAT,
+  return_rate FLOAT
 );
 
 CREATE TABLE `Container`(
   code INTEGER NOT NULL PRIMARY KEY,
-  item_code INTEGER FOREIGN KEY REFERENCES Item(code),
+  item_code INTEGER,
   capacity FLOAT,
-  amount FLOAT
+  amount FLOAT,
+  FOREIGN KEY fk_item(item_code) REFERENCES Item(code)
 );
 
 CREATE TABLE `Formula`(
   id INTEGER NOT NULL PRIMARY KEY,
   code INTEGER NOT NULL,
-  fabricated_item_code INTEGER FOREIGN KEY REFERENCES Item(code),
+  fabricated_item_code INTEGER ,
   amount FLOAT,
-  item INTEGER FOREIGN KEY REFERENCES Item(code)
+  item INTEGER,
+  FOREIGN KEY fk_item(item) REFERENCES Item(code),
+  FOREIGN KEY fk_fab(fabricated_item_code) REFERENCES Item(code)
 );
 
 CREATE TABLE `Fabrication`(
-  id INTEGER NOT NULL,
+  id INTEGER NOT NULL PRIMARY KEY,
   fabric_date DATE,
   fabric_item INTEGER
 );
 
 CREATE TABLE `FabricationItem` (
-  id INTEGER NOT NULL  FOREIGN KEY REFERENCES Fabrication(code),
+  id INTEGER NOT NULL PRIMARY KEY,
+  fabric_id INTEGER NOT NULL,
   quantity FLOAT,
-  item INTEGER NOT NULL  FOREIGN KEY REFERENCES Item(id)
+  item INTEGER NOT NULL,
+  container_id INTEGER NOT NULL,
+  FOREIGN KEY fk_fab(fabric_id) REFERENCES Fabrication(id),
+  FOREIGN KEY fk_it(item) REFERENCES Item(code),
+  FOREIGN KEY fk_cont(container_id) REFERENCES Container(code)
 );
 
 CREATE TABLE `Transfer`(
   id INTEGER NOT NULL,
   transfer_date DATE,
-  container INTEGER FOREIGN KEY REFERENCES Container(code),
+  container INTEGER,
   quantity FLOAT,
-  dest_container INTEGER FOREIGN KEY REFERENCES Container(code),
-  client INTEGER FOREIGN KEY REFERENCES Client(id)
+  dest_container INTEGER,
+  client INTEGER,
+  FOREIGN KEY fk_cont(container) REFERENCES Container(code),
+  FOREIGN KEY fk_dest(dest_container) REFERENCES Container(code),
+  FOREIGN KEY fk_cli(client) REFERENCES Client(id)
+
 );
 
 CREATE TABLE `Leak`(
   id INTEGER NOT NULL PRIMARY KEY,
   leak_date DATE,
-  item INTEGER FOREIGN KEY REFERENCES Item(code),
+  item INTEGER ,
   quantity FLOAT,
-  container INTEGER FOREIGN KEY REFERENCES Container(code)
+  container INTEGER ,
+  FOREIGN KEY fk_it(item) REFERENCES Item(code),
+  FOREIGN KEY fk_cont(container) REFERENCES Container(code)
 );
 
 CREATE TABLE `Buy`(
   id INTEGER NOT NULL PRIMARY KEY,
   buy_date DATE,
-  item INTEGER FOREIGN KEY REFERENCES Item(code),
-  supplier INTEGER FOREIGN KEY REFERENCES Supplier(id),
+  item INTEGER,
+  supplier INTEGER,
   quantity FLOAT,
-  dest_container INTEGER FOREIGN KEY REFERENCES Container(code)
+  dest_container INTEGER ,
+  FOREIGN KEY fk_it(item) REFERENCES Item(code),
+  FOREIGN KEY fk_sup(supplier) REFERENCES Supplier(id),
+  FOREIGN KEY fk_dest(dest_container) REFERENCES Container(code)
 );
 
 CREATE TABLE `Sale`(
   id INTEGER NOT NULL PRIMARY KEY,
   sale_date DATE,
-  item INTEGER FOREIGN KEY REFERENCES Item(code),
+  item INTEGER,
+  client INTEGER,
+  container INTEGER,
   quantity FLOAT,
-  client INTEGER FOREIGN KEY REFERENCES Client(id),
-  container INTEGER FOREIGN KEY REFERENCES Container(code),
-  quantity FLOAT
-);
-
-CREATE TABLE `Client`(
-  id INTEGER NOT NULL PRIMARY KEY,
-  name VARCHAR
-);
-
-CREATE TABLE `Supplier`(
-  id INTEGER NOT NULL PRIMARY KEY,
-  name VARCHAR
+  FOREIGN KEY fk_it(item) REFERENCES Item(code),
+  FOREIGN KEY fk_cl(client) REFERENCES Client(id),
+  FOREIGN KEY fk_cont(container) REFERENCES Container(code)
 );
